76a7b842a08e20a4edd86a45303ae9346ca02726
Carlo Debernardi <carlo.debernardi.w@gmail.com>
2026-03-24 11:44:31 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -180,8 +180,6 @@ plt.close()
 
 ##
 
-stoppe
-
 su1 = az.summary(idata, var_names=["sh"])
 su2 = az.summary(idata2, var_names=["sh"])
 x = np.arange(su1.shape[0])
@@ -212,7 +210,159 @@ plt.savefig('figs/m2_gammas.png', dpi=140, bbox_inches='tight')
 plt.show()
 plt.close()
 
-y_rep = np.array(az.extract(idata, var_names="y_rep"))
+##
+
+y_rep = np.array(az.extract(idata2, var_names="y_rep"))
 
 sec = np.array(data['sector'])
 y_obs = np.array(data['y'])
+
+##
+
+mask = (sec == 16)
+x = np.arange(8)
+bvals = []
+for i in x:
+    vals = y_rep[mask,i,:].flatten()
+    bvals.append(vals)
+    plt.hlines(y_obs[mask,i], i-0.45, i+0.45, color='tab:red', lw=1, zorder=5)
+plt.boxplot(bvals, positions=x, patch_artist=True, widths=0.7,
+            showmeans=False, showfliers=False,
+            medianprops={"color": "white", "linewidth": 0.5},
+            boxprops={"facecolor": "grey", "edgecolor": "white",
+                      "linewidth": 0.5},
+            whiskerprops={"color": "grey"},
+            capprops={"color": "grey"}, zorder=4)
+plt.yscale('log')
+plt.grid(alpha=0.3)
+plt.title('Metalworking')
+plt.xticks(x, labels=x+1)
+plt.xlabel('Severity classes')
+plt.ylabel('Reported cases (log)')
+plt.savefig('figs/m2_metal.png', dpi=140, bbox_inches='tight')
+plt.show()
+plt.close()
+
+###############################################################################
+
+sev = 2
+x = np.arange(data['S'])
+bvals = []
+for s in x:
+    mask = (sec == s+1)
+    vals = y_rep[mask,sev,:].flatten()
+    bvals.append(vals)
+    plt.hlines(y_obs[mask,sev], s-0.45, s+0.45, color='tab:red', lw=1, zorder=5)
+plt.boxplot(bvals, positions=x, patch_artist=True, widths=0.7,
+            showmeans=False, showfliers=False,
+            medianprops={"color": "white", "linewidth": 0.5},
+            boxprops={"facecolor": "grey", "edgecolor": "white",
+                      "linewidth": 0.5},
+            whiskerprops={"color": "grey"},
+            capprops={"color": "grey"}, zorder=4)
+plt.title('Severity class 3')
+plt.xticks(x, labels=x+1)
+plt.grid(alpha=0.3)
+plt.ylabel('Number of injuries')
+plt.xlabel('Subsectors')
+plt.savefig('figs/m2_sc3.png', dpi=140, bbox_inches='tight')
+plt.show()
+plt.close()
+
+###############################################################################
+
+# MODEL 2
+
+model = CmdStanModel(stan_file="model3.stan")
+
+# --- Sample from the model ---
+fit = model.sample(
+    data=data,
+    chains=4,
+    parallel_chains=4,
+    iter_warmup=1000,
+    iter_sampling=1000,
+    show_console=True
+)
+
+# --- Show summary ---
+# s = fit.summary()
+# print(s)
+
+idata3 = az.from_cmdstanpy(fit)
+
+s3 = az.summary(idata3, var_names=["alpha","sh","sh_sigma","sigma_incidence","log_incidence"])
+s3.to_csv('figs/m3_summary.csv')
+
+az.plot_trace(idata3, var_names=['alpha'])
+plt.show()
+plt.close()
+
+az.plot_trace(idata3, var_names=['sh'])
+plt.show()
+plt.close()
+
+az.plot_trace(idata3, var_names=['sh_sigma'])
+plt.show()
+plt.close()
+
+az.plot_trace(idata3, var_names=['log_incidence'])
+plt.show()
+plt.close()
+
+##
+
+suf = az.summary(idata3, var_names=["log_incidence"])
+suf['sector'] = (suf.index.str.split('[').str[1]
+                          .str.split(',').str[0].astype(int))
+
+selected_sectors = np.array([1, 7, 13, 16, 19, 24])-1
+x = np.arange(data['K'])
+
+for i,s in enumerate(selected_sectors):
+    su = suf[suf.sector == s]
+    off = i / (selected_sectors.shape[0] + 2)
+    plt.scatter(x+off, np.exp(su['mean']), color=f'C{s}', label=s+1)
+    plt.plot([x+off,x+off], np.exp([su['hdi_3%'], su['hdi_97%']]), color=f'C{s}')
+plt.xticks(x, labels=x+1)
+plt.grid(alpha=0.3)
+plt.yscale('log')
+plt.title('Incidence by severity, selected sectors')
+plt.xlabel('Severity class')
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.savefig('figs/m3_incid_sec.png', dpi=140, bbox_inches='tight')
+plt.show()
+plt.close()
+
+# ##
+
+y_rep = np.array(az.extract(idata3, var_names="y_rep"))
+
+sec = np.array(data['sector'])
+y_obs = np.array(data['y'])
+
+# ###############################################################################
+
+sev = 2
+x = np.arange(data['S'])
+bvals = []
+for s in x:
+    mask = (sec == s+1)
+    vals = y_rep[mask,sev,:].flatten()
+    bvals.append(vals)
+    plt.hlines(y_obs[mask,sev], s-0.45, s+0.45, color='tab:red', lw=1, zorder=5)
+plt.boxplot(bvals, positions=x, patch_artist=True, widths=0.7,
+            showmeans=False, showfliers=False,
+            medianprops={"color": "white", "linewidth": 0.5},
+            boxprops={"facecolor": "grey", "edgecolor": "white",
+                      "linewidth": 0.5},
+            whiskerprops={"color": "grey"},
+            capprops={"color": "grey"}, zorder=4)
+plt.title('Severity class 3')
+plt.xticks(x, labels=x+1)
+plt.grid(alpha=0.3)
+plt.ylabel('Number of injuries')
+plt.xlabel('Subsectors')
+plt.savefig('figs/m3_sc3.png', dpi=140, bbox_inches='tight')
+plt.show()
+plt.close()
